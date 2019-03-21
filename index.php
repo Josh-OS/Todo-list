03db0f15886243652f8b7e3827040a9a7d765c7e
Josh-OS <skybyte4me@gmail.com>
2019-03-21 20:51:13 +0800

    Styling - Base pad design

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>Task To Do</title>
+  <link rel="stylesheet" type="text/css" href="todocss.css">
+ </head>
+</html>
+
+<body>
+ <div class="pad">
+  <ul>First task</ul>
+  <ul>Second task</ul>
+  <ul>Third task</ul>
+ </div>
