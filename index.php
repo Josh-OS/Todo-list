a693e7771b1a3ca81d648fddce30be108380215a
Josh-OS <skybyte4me@gmail.com>
2019-03-22 16:21:58 +0800

    CSV to array added

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+
+
 <html>
  <head>
   <title>Task To Do</title>
@@ -11,14 +13,17 @@
  <div class="pad">
  <h3 class="title">To Do</h3>
  
- <!--Task list-->
-  <div class="todo-list">
-  <ul>
-    <li>First task</li>
-    <li>Second task</li>
-    <li>Third task</li>
-  </ul>
-  </div>
+<!--Todo list (table)-->
+<?php
+  echo '<!--Task list-->';
+  echo '<div class="todo-list">';
+  echo '<ul>';
+  echo '  <li>First task</li>';
+  echo '  <li>Second task</li>';
+  echo '  <li>Third task</li>';
+  echo ' </ul>';
+  echo '</div>';
+?>
 
   <div class="adder">
    <form action="" method="get">
@@ -35,8 +40,29 @@
 <?php
 
 //Add btn is clicked.
-if ($_GET['addbox']) {
-	$container = $_GET["addbox"];
-		echo $container;
-		echo " made alive by Jesus";
+  if ($_GET['addbox']) {
+
+	//TO CSV
+	$container = $_GET; //GET to array
+	$container['id'] = uniqid(); //append generated id
+
+	file_put_contents('file_todo.csv', (implode(',', $container) . "\n"), FILE_APPEND);
+
+	//CSV TO SCREEN
+	$container = file_get_contents('file_todo.csv');
+	$lister =[];
+
+	foreach(explode("\n", $container) as $container) { //string to organized array
+		[$id, $task_list] = explode(',', $container);
+
+		$lister[] = [
+			'id' => $id,
+			'task_list' => $task_list
+		];
+	}
+	var_dump($lister);
+	echo "$task_list";
+
 }
+
+	
