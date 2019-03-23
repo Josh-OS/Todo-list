ba1140bb91038e745a3c8e4eda9f7f97780b138c
Josh-OS <skybyte4me@gmail.com>
2019-03-23 14:08:51 +0800

    failed

--- index.php
+++ index.php
@@ -15,14 +15,21 @@
  
 <!--Todo list (table)-->
 <?php
-  echo '<!--Task list-->';
-  echo '<div class="todo-list">';
-  echo '<ul>';
-  echo '  <li>First task</li>';
-  echo '  <li>Second task</li>';
-  echo '  <li>Third task</li>';
-  echo ' </ul>';
-  echo '</div>';
+	//CSV TO SCREEN
+	echo '<div class="todo-list">';
+	$container = file_get_contents('file_todo.csv');
+	$lister =[];
+
+	foreach(explode("\n", $container) as $contains) { //string to organized array
+		[$id, $task_list] = explode(',', $contains);
+
+		$lister[] = [
+			'id' => $id,
+			'task_list' => $task_list
+		];
+	}
+	echo $lister['id'];
+	echo '</div>';
 ?>
 
   <div class="adder">
@@ -48,21 +55,9 @@
 
 	file_put_contents('file_todo.csv', (implode(',', $container) . "\n"), FILE_APPEND);
 
-	//CSV TO SCREEN
-	$container = file_get_contents('file_todo.csv');
-	$lister =[];
 
-	foreach(explode("\n", $container) as $container) { //string to organized array
-		[$id, $task_list] = explode(',', $container);
+  }
 
-		$lister[] = [
-			'id' => $id,
-			'task_list' => $task_list
-		];
-	}
-	var_dump($lister);
-	echo "$task_list";
-
-}
+?>
 
 	
